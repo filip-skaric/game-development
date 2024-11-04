f46f767dcf568bb3e3a84d72e808f97dc9bb68ad
Filip Skaric <filip.skaric@gmail.com>
2024-11-04 13:47:17 +0100

    Main method updated with jFrame

--- src/com/fico/BrickBreaker/Main.java
+++ src/com/fico/BrickBreaker/Main.java
@@ -1,9 +1,17 @@
 package com.fico.BrickBreaker;
 
+import javax.swing.JFrame;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		JFrame obj = new JFrame();
+		GamePlay gamePlay = new GamePlay();
+		obj.setBounds(10, 10, 700, 600); //this are dimension of the app
+		obj.setTitle("Brick Breaker"); //title
+		obj.setResizable(false); //not resizable
+		obj.setVisible(true); 
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //when press close in app
 	}
 }
