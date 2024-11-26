5a3d38678dbca5135787c4d10aa425232e636968
Filip Skaric <filip.skaric@gmail.com>
2024-11-26 16:41:11 +0100

    commnets on main class added + snake game development

--- src/com/fico/SnakeFeeder/GamePlay.java
+++ src/com/fico/SnakeFeeder/GamePlay.java
@@ -1,5 +1,41 @@
 package com.fico.SnakeFeeder;
 
-public class GamePlay {
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+public class GamePlay extends JPanel {
+
+	private ImageIcon titleImage;
+	
+	public GamePlay() {
+
+		//titleImage = new ImageIcon("src/com/fico/SnakeFeeder/snake.jpg");
+	}
+	
+	public void paint (Graphics g) {
+		
+        // Display title
+		titleImage = new ImageIcon("src/com/fico/SnakeFeeder/resource/title.png");
+		Image scaledImage = titleImage.getImage().getScaledInstance(850, 75, Image.SCALE_SMOOTH);
+
+		// Ensure the image is fully loaded before drawing
+		ImageIcon scaledIcon = new ImageIcon(scaledImage); 
+		g.drawImage(scaledIcon.getImage(), 25, 5, this);
+		
+		
+		//display gameplay border
+		g.setColor(Color.DARK_GRAY);
+		g.drawRect(24, 74, 851, 577);
+		
+		//Display gameplay background
+		g.setColor(Color.BLACK);
+		g.fillRect(25, 75, 850, 575);
+		
+		g.dispose();
+		
+	}
 }
--- src/com/fico/SnakeFeeder/Main.java
+++ src/com/fico/SnakeFeeder/Main.java
@@ -1,10 +1,32 @@
 package com.fico.SnakeFeeder;
 
+import java.awt.Color;
+import javax.swing.JFrame;
+
+
+/**
+ * The Main class is the entry point for the Snake Feeder game application. It
+ * sets up the main game window and initializes the game play.
+ */
 public class Main {
 
+	/**
+	 * The main method sets up the JFrame for the Snake Feeder game. It configures
+	 * the window properties and adds the GamePlay component.
+	 *
+	 * @param args command-line arguments (not used)
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		JFrame obj = new JFrame(); // Creates a new JFrame object for the game window
+		GamePlay gamePlay = new GamePlay(); // Creates a new GamePlay object which contains the game logic
+		obj.setBounds(10, 10, 905, 700); // Sets the position and size of the window
+		obj.setBackground(Color.DARK_GRAY); // Sets the background color of the window
+		obj.setResizable(false); // Disables window resizing
+		obj.setVisible(true); // Makes the window visible
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Ensures the application exits when the window is closed
+		obj.setTitle("Snake game"); // Sets the title of the window
+		obj.add(gamePlay); // Adds the GamePlay component to the window
+		
 	}
-
 }
