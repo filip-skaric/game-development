aaf70bb26790cb0f45b0e116474b564f51ef8aa8
Filip Skaric <filip.skaric@gmail.com>
2024-11-26 16:41:11 +0100

    commnets on main class added + snake game development

--- src/com/fico/BrickBreaker/Main.java
+++ src/com/fico/BrickBreaker/Main.java
@@ -1,18 +1,28 @@
+
 package com.fico.BrickBreaker;
 
 import javax.swing.JFrame;
 
+/**
+ * The Main class is the entry point for the Brick Breaker game application.
+ * It sets up the main game window and initializes the game play.
+ */
 public class Main {
 
-	public static void main(String[] args) {
-
-		JFrame obj = new JFrame();
-		GamePlay gamePlay = new GamePlay();
-		obj.setBounds(10, 10, 700, 600); //this are dimension of the app
-		obj.setTitle("Brick Breaker"); //title
-		obj.setResizable(true); //not resizable
-		obj.setVisible(true); 
-		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //when press close in app
-		obj.add(gamePlay);
-	}
+    /**
+     * The main method sets up the JFrame for the Brick Breaker game.
+     * It configures the window properties and adds the GamePlay component.
+     *
+     * @param args command-line arguments (not used)
+     */
+    public static void main(String[] args) {
+    	
+    	JFrame obj = new JFrame(); // Creates a new JFrame object for the game window
+        GamePlay gamePlay = new GamePlay(); // Creates a new GamePlay object which contains the game logic
+        obj.setBounds(10, 10, 700, 600); // Sets the position and size of the window
+        obj.setTitle("Brick Breaker"); // Sets the title of the window
+        obj.setResizable(true); // Allows the window to be resizable
+        obj.setVisible(true); // Makes the window visible
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Ensures the application exits when the window is closed
+    }
 }
